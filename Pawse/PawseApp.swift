0952bf08e903446a6895478474c65b2e17abd924
Hendra Lijaya <hendralijaya@gmail.com>
2024-06-21 20:53:57 +0700

    Initial Commit

---
+++ Pawse/PawseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawseApp.swift
+//  Pawse
+//
+//  Created by hendra on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PawseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
