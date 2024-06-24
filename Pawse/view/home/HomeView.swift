ed876313fb56f44051f1ac2928c859ef12b31665
muhammad.akmal14 <akmalhakim2106@gmail.com>
2024-06-24 16:01:29 +0700

    akmal/add home and shop

--- Pawse/view/home/HomeView.swift
+++ Pawse/view/home/HomeView.swift
@@ -8,30 +8,14 @@
 import SwiftUI
 
 struct HomeView: View {
-    @EnvironmentObject var authViewModel: AuthenticationViewModel
-    @EnvironmentObject var route: Route
+//    @EnvironmentObject var authViewModel: AuthenticationViewModel
+//    @EnvironmentObject var route: Route
     var body: some View {
-        NavigationStack(path: $route.pawsePath) {
-            VStack {
-                Text("Home")
-                Text(authViewModel.displayName)
-            }
-            .toolbar(content: {
-                HStack{
-                    Image(systemName: "cross.circle.fill")
-                    Text("Logout")
-                }
-                .onTapGesture {
-                    authViewModel.signOut()
-                }
-            })
-//            Navigation from Home
-//            .navigationDestination(for: PawseRoute.self) { route in
-//                switch route {
-//                case
-//                }
-//            }
+        VStack {
+            CatView()
+            QuestView()
         }
+        .padding(.horizontal, 16)
     }
 }
 
